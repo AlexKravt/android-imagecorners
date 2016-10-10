31d4054c3bd26e44365f67e467c2e1754a87143e
Kravtsov.a <kravtsov.a@ldf.ru>
2016-10-10 11:32:56 +0300

    Update Project

--- OvalImage/app/src/main/java/p1/ovalimage/Views/OvalImageView.java
+++ OvalImage/app/src/main/java/p1/ovalimage/Views/OvalImageView.java
@@ -28,90 +28,97 @@ public class OvalImageView extends ImageView {
     public OvalImageView(Context context) {
         super(context);
         // TODO Auto-generated constructor stub
-        mask = BitmapFactory.decodeResource(context.getResources(), R.drawable.oval_fore4);
+        initBitmapMask(context);
     }
 
     public OvalImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mask = BitmapFactory.decodeResource(context.getResources(), R.drawable.oval_fore4);
+        initBitmapMask(context);
     }
 
     public OvalImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mask = BitmapFactory.decodeResource(context.getResources(), R.drawable.oval_fore4);
+        initBitmapMask(context);
     }
 
+    private void initBitmapMask(Context context)
+    {
+        mask = BitmapFactory.decodeResource(context.getResources(), R.drawable.oval_fore4);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        Drawable drawable = getDrawable();
+        Drawable drawable = getDrawable(); //<-- Если есть картинка из ресурса, то получаем
 
-        if (drawable == null) {
+        if (drawable == null) { //<-- Если нет, то выходим
             return;
         }
 
-        if (getWidth() == 0 || getHeight() == 0) {
+        if (getWidth() == 0 || getHeight() == 0) { // <-- Или картинка не имеет размера, также выходим
             return;
         }
 
-        Bitmap b = null;
+        Bitmap bitmap; //<-- создаем переменную
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
-                && drawable instanceof VectorDrawable) {
+                && drawable instanceof VectorDrawable) //<-- Проверка версии SDK
+        {
+            //Получаем картинку
             ((VectorDrawable) drawable).draw(canvas);
-            b = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
-            Canvas c = new Canvas();
-            c.setBitmap(b);
-            drawable.draw(c);
+            bitmap = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas_new = new Canvas();
+            canvas_new.setBitmap(bitmap);
+            drawable.draw(canvas_new);
         }
         else
         {
-
-            b = ((BitmapDrawable) drawable).getBitmap();
+            //Получаем картинку
+            bitmap = ((BitmapDrawable) drawable).getBitmap();
         }
 
-        Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
 
-        int w = getWidth(), h = getHeight();
+        int width = getWidth(), height = getHeight();
 
-       /* Bitmap roundBitmap =  getCroppedBitmap(bitmap, w);
-        canvas.drawBitmap(roundBitmap, 0,0, null);*/
+        //Копируем картинку для вырезки квадратной части. Другими словами для crop
+        /* Bitmap bitmap_new = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+           Bitmap cropBitmap =  getCroppedBitmap(bitmap_new, width);
+           canvas.drawBitmap(cropBitmap, 0,0, null);*/
 
 
-        Bitmap roundBitmap =  getRoundOval(bitmap, mask);
+        Bitmap roundBitmap =  getRoundOval(bitmap, mask);//<-- Создаём картинку с наложением маски
+       //Делаем канву из картинки для вывода на экран
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        RectF rectF = new RectF(0, 0,w,w);
+        RectF rectF = new RectF(0, 0,width,width);
         paint.setStyle(Paint.Style.FILL);
         canvas.drawBitmap(roundBitmap, null, rectF, paint);
 
-        roundBitmap.recycle();
-       // mask.recycle();
+        roundBitmap.recycle(); // <-- Удаляем картинку после вывода
+
     }
 
     public static Bitmap getRoundOval(Bitmap original,Bitmap mask)
     {
-        // Bitmap mask = BitmapFactory.decodeResource(getResources(),R.drawable.oval_fore3);
-
-        //You can change original image here and draw anything you want to be masked on it.
-        Bitmap result  = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
-            Canvas tempCanvas = new Canvas(result);
+        //Создание изображения с наложением маски
+        Bitmap bitmap_new  = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas tempCanvas = new Canvas(bitmap_new);
             Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
             RectF rectF = new RectF(0, 0,mask.getWidth(), mask.getHeight());
             paint.setStyle(Paint.Style.FILL);
+        //Сначала обрабатываем оригинал
             tempCanvas.drawBitmap(original, null, rectF, paint);
             paint.setXfermode(null);
             paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        //Затем накладываем маску
             tempCanvas.drawBitmap(mask, 0, 0, paint);
             paint.setXfermode(null);
 
-            //Draw result after performing masking
-            tempCanvas.drawBitmap(result, 0, 0, new Paint());
-
+        //Получаем рзультат
+            tempCanvas.drawBitmap(bitmap_new, 0, 0, new Paint());
 
         // mask.recycle();
-        // original.recycle();
+         original.recycle();// <-- если не используется в списках типа ListView и т.д. иначе закомментировать
 
-        return result;
+        return bitmap_new;
 
 
     }
